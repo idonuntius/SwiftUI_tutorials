8f32e7115ce427a9ee268990a257d15b90c4833d
idonun <mc.natsu831@gmail.com>
2020-07-25 20:28:55 +0900

    first commit

---
+++ SwiftUI_tutorials/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_tutorials
+//
+//  Created by NatsukiIdota on 2020/07/25.
+//  Copyright © 2020 idonuntius. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
